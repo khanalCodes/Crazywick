adccb9e21a0d325c104d2e7d3cd9577461166def
Manish <khanalmanish984@gmail.com>
2026-05-10 11:27:59 +0545

    Totally frustrated with this admin panel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https: blob:",
+              "connect-src 'self' https://accounts.google.com https://*.neon.tech",
+              "frame-src https://accounts.google.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
